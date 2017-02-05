7ed02ddf617048e7601bd36b6954702ad8a3882f
Gama11 <jensfischer95@gmail.com>
2017-02-05 22:29:06 +0100

    Highlight __global__ and __this__ as keywords

--- cases/UntypedFunctions.hx
+++ cases/UntypedFunctions.hx
@@ -1,3 +1,8 @@
 untyped __js__('alert("Haxe is great!")');
 untyped __php__("echo '<pre>'; print_r($value); echo '</pre>';");
 untyped __call__("array", 1,2,3);
+untyped __global__.__hxcpp_dbg_setNewStackFrameFunction
+untyped __global__["flash.utils.setTimeout"]
+next : function() {
+    return __this__.arr[__this__.cur++];
+}
