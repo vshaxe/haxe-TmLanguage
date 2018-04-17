aaf2245c7ffd1ba9d58cef8d6866777e50da7b7b
Jens Fischer <jensfischer95@gmail.com>
2018-04-17 15:12:06 +0200

    Fix const type param regex literals not being highlighted, closes #37

--- cases/ConstantTypeParams.hx
+++ cases/ConstantTypeParams.hx
@@ -1,3 +1,4 @@
 typedef WinHandle = hl.Abstract<"ui_window">;
 typedef WinHandle = hl.Abstract<@:const 5>;
 typedef WinHandle = hl.Abstract<["foo", 5]>;
+typedef WinHandle = hl.Abstract<~/^hell[o] (world)$/>;
