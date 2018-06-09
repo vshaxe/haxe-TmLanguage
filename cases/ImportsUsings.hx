9ac248dee2aff011eac5a386bcbdf30c41dc81d5
Jens Fischer <jensfischer95@gmail.com>
2018-06-09 17:59:38 +0200

    Replace the package/import/using regexes with something sane
    ...and correct!

--- cases/ImportsUsings.hx
+++ cases/ImportsUsings.hx
@@ -6,12 +6,19 @@ import net.onthewings.*;
 import net.onthewings.Test.*;
 import _net.onthewings.Test.field;
 
+import Type.SubType;
+import pack.Type.SubType;
+import pack.Type.Enum.Field;
+
 import _ha_xe.Int64 as I64;
+import _ha_xe.Int64 as
 import haxe.Int64 in I64;
 import haxe.Int64 in __Int64;
 import haxe.macro.Expr;
 import haxe.__Int64;
 import haxe.__Int64.field;
+
+import /**/ _ha_xe.Int64 /**/ as /**/ I64;
 
 using Lambda;
 using _ne_t.onthewings.__Int64;
