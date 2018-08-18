26a6e1a7191e7d41adfcfdca7475c81b4a51f4ff
Jens Fischer <jensfischer95@gmail.com>
2018-08-18 19:39:38 +0200

    Fix comments + conditionals in property declarations

--- cases/Properties.hx
+++ cases/Properties.hx
@@ -3,3 +3,4 @@ private var attr(get, set):Dynamic<Float> = 1;
 private var attr2(default, null) = ['Test'];
 private var attr3(dynamic, never);
 var group(get_group, set_group);
+public static var tweens(#if haxe3 get #else get_tweens #end , /* foo */ never):Iterable<TweenX>;
