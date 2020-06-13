c2c5c2261dc576f28458fede0d6255e3c213bc20
Jens Fischer <jensfischer95@gmail.com>
2020-06-14 00:37:19 +0200

    Use module-level fields

--- src/Converter.hx
+++ src/Converter.hx
@@ -1,30 +1,28 @@
 import sys.io.File;
 
-class Converter {
-	static function main() {
-		Sys.print("Converting syntax files to plist...");
+function main() {
+	Sys.print("Converting syntax files to plist...");
 
-		convert("haxe.YAML-tmLanguage", "haxe.tmLanguage");
-		convert("hxml.YAML-tmLanguage", "hxml.tmLanguage");
+	convert("haxe.YAML-tmLanguage", "haxe.tmLanguage");
+	convert("hxml.YAML-tmLanguage", "hxml.tmLanguage");
 
-		convert("helper/haxe-type.YAML-tmLanguage", "helper/haxe-type.tmLanguage");
-		convert("helper/haxe-argument.YAML-tmLanguage", "helper/haxe-argument.tmLanguage");
-		convert("helper/haxe-markdown.YAML-tmLanguage", "helper/haxe-markdown.tmLanguage");
-		convert("helper/hxml-markdown.YAML-tmLanguage", "helper/hxml-markdown.tmLanguage");
-		convert("helper/haxe-markdown-docs.YAML-tmLanguage", "helper/haxe-markdown-docs.tmLanguage");
+	convert("helper/haxe-type.YAML-tmLanguage", "helper/haxe-type.tmLanguage");
+	convert("helper/haxe-argument.YAML-tmLanguage", "helper/haxe-argument.tmLanguage");
+	convert("helper/haxe-markdown.YAML-tmLanguage", "helper/haxe-markdown.tmLanguage");
+	convert("helper/hxml-markdown.YAML-tmLanguage", "helper/hxml-markdown.tmLanguage");
+	convert("helper/haxe-markdown-docs.YAML-tmLanguage", "helper/haxe-markdown-docs.tmLanguage");
 
-		Sys.println(" Done.\n ");
-	}
+	Sys.println(" Done.\n ");
+}
 
-	static function convert(source:String, dest:String) {
-		var content = File.getContent(source);
-		var parsed = new yaml.Parser().parse(content, new yaml.Parser.ParserOptions().useObjects());
+private function convert(source:String, dest:String) {
+	var content = File.getContent(source);
+	var parsed = new yaml.Parser().parse(content, new yaml.Parser.ParserOptions().useObjects());
 
-		switch plist.Writer.write(parsed) {
-			case Success(data):
-				File.saveContent(dest, data);
-			case _:
-				throw "Error";
-		}
+	switch plist.Writer.write(parsed) {
+		case Success(data):
+			File.saveContent(dest, data);
+		case _:
+			throw "Error";
 	}
 }
--- src/Test.hx
+++ src/Test.hx
@@ -5,32 +5,30 @@ import sys.io.File;
 using StringTools;
 
 // ported and adapted from https://github.com/Microsoft/TypeScript-TmLanguage
-class Test {
-	static inline var BASELINES_DIR = "baselines";
+private inline final BASELINES_DIR = "baselines";
 
-	static function main() {
-		var hasError = false;
-		for (file in FileSystem.readDirectory(GENERATED_DIR)) {
-			var generatedText = File.getContent('$GENERATED_DIR/$file');
-			var baseline = '$BASELINES_DIR/$file';
-			if (!FileSystem.exists(baseline)) {
-				hasError = true;
-				Sys.println('Warning: "$file" has no baseline.');
-				continue;
-			}
-			var baselinesText = File.getContent(baseline);
-			if (removeNewlines(generatedText) != removeNewlines(baselinesText)) {
-				hasError = true;
-				Sys.println('Error: "$file" is not the same as the baseline!');
-			}
+function main() {
+	var hasError = false;
+	for (file in FileSystem.readDirectory(GENERATED_DIR)) {
+		var generatedText = File.getContent('$GENERATED_DIR/$file');
+		var baseline = '$BASELINES_DIR/$file';
+		if (!FileSystem.exists(baseline)) {
+			hasError = true;
+			Sys.println('Warning: "$file" has no baseline.');
+			continue;
 		}
-
-		Sys.println(" \n" + if (hasError) "Failed!" else "Success.");
-		if (hasError)
-			Sys.exit(1);
+		var baselinesText = File.getContent(baseline);
+		if (removeNewlines(generatedText) != removeNewlines(baselinesText)) {
+			hasError = true;
+			Sys.println('Error: "$file" is not the same as the baseline!');
+		}
 	}
 
-	static function removeNewlines(text:String):String {
-		return text.replace('\r\n', '').replace('\n', '');
-	}
+	Sys.println(" \n" + if (hasError) "Failed!" else "Success.");
+	if (hasError)
+		Sys.exit(1);
+}
+
+private function removeNewlines(text:String):String {
+	return text.replace('\r\n', '').replace('\n', '');
 }
