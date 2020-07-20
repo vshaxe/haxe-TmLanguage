06c369275b7b1bd24049637481cc7d6d88074f72
Jens Fischer <jensfischer95@gmail.com>
2020-07-20 22:41:49 +0200

    Add support for overload modifiers
    HaxeFoundation/haxe#9729

---
+++ cases/Overload.hx
@@ -0,0 +1,3 @@
+overload static function itWasYou(i1:Int, i2:Int):String;
+overload static function itWasYou(s1:String, s2:String):String;
+overload static function itWasYou(f1:Float, f2:Float):String;
