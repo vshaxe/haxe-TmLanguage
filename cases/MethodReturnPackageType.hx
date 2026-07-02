7f2e6feda8aa74ae5edebb04607f63f762bfc365
RblSb <msrblsb@gmail.com>
2026-07-02 05:53:38 +0300

    Fix type path method return type hints

---
+++ cases/MethodReturnPackageType.hx
@@ -0,0 +1,10 @@
+class Main {
+	function a():haxe.ds.Vector<Int> {
+		return null;
+	}
+	function b():haxe.io.Bytes return null;
+	function c():sys.io.File {}
+	function d() : foo.Bar {}
+	function e() : foo.Bar
+		return null;
+}
